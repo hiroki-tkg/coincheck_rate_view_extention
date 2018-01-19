f04dc5b9e07305388e58cca5f1e733c7ff4e7fff
Hiroki Takagi <tkg.japan@gmail.com>
2018-01-19 13:00:51 +0700

    buyページで実際のレートを表示

---
+++ popup.js
@@ -0,0 +1,10 @@
+$(function(){
+	'use strict';
+
+	document.addEventListener('DOMContentLoaded', function() {
+	    var link = document.getElementsByClassName('load-more-button');
+	    link.addEventListener('click', function() {
+	        alert('tttt');
+	    });
+	});
+});
